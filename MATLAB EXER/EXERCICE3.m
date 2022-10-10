4cccc2bd82e044decd83fef4197d51413ec4330d
MOUSTAHSSINEintissar <115045626+MOUSTAHSSINEintissar@users.noreply.github.com>
2022-10-10 23:28:49 +0100

    Add files via upload

---
+++ MATLAB EXER/EXERCICE3.m
@@ -0,0 +1,12 @@
+A=input('donner age d enfant')
+if 6<=A && A<=7
+    disp('la catégorie sportive d un enfant est un Poussin')
+elseif 8<=A && A<=9
+    disp('la catégorie sportive d un enfant est un Pupille')
+elseif 10<=A && A<=11
+     disp('la catégorie sportive d un enfant est un Minime')
+elseif 12<=A && A<=14
+    disp('la catégorie sportive d un enfant est un Cadet')
+else
+    disp('l age n est pas valide')
+end
